1e600863ad8f3a8cc15367e635cee3ebe16da4f6
BogdanATA <batameev@gmail.com>
2026-04-21 14:57:04 -0400

    - created printout for EEEE, MMM dd, yyyy HH:mm (prints out gmt time at end

--- src/main/java/com/pluralsight/FormatDates.java
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -2,6 +2,8 @@ package com.pluralsight;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class FormatDates {
@@ -9,6 +11,14 @@ public class FormatDates {
         // current date
         LocalDate today = LocalDate.now();
 
+        // current date + time
+        LocalDateTime now = LocalDateTime.now();
+
+        //current date + time in GMT
+        ZonedDateTime gmtTime = ZonedDateTime.now(ZoneId.of("GMT"));
+
+
+        //print out formated date and time
         DateTimeFormatter format1 = DateTimeFormatter.ofPattern("MM/dd/yyyy");
         System.out.println(today.format(format1));
 
@@ -18,9 +28,8 @@ public class FormatDates {
         DateTimeFormatter format3 = DateTimeFormatter.ofPattern("EEEE MM, yyyy");
         System.out.println(today.format(format3));
 
-        // current date + time
-        LocalDateTime now = LocalDateTime.now();
-
+        DateTimeFormatter format4 = DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy HH:mm");
+        System.out.println(gmtTime.format(format4));
 
     }
 }
