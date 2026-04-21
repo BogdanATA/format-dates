96a8cdb5502b1b0388d7c2904acbded1907b6b95
BogdanATA <batameev@gmail.com>
2026-04-21 14:34:03 -0400

    - created print out for current date in MM/dd/yyyy format

--- src/main/java/com/pluralsight/FormatDates.java
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -1,4 +1,20 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class FormatDates {
+    public static void main(String[] args) {
+        // current date
+        LocalDate today = LocalDate.now();
+
+        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        System.out.println(today.format(format1));
+
+        // current date + time
+        LocalDateTime now = LocalDateTime.now();
+
+
+    }
 }
